794bc18ca0f6352e18dcc5353cbc965413d7695e
rynnchan <ogw334@gmail.com>
2020-07-07 22:19:11 +0900

    chapter3-24

---
+++ chapter3/3-24.py
@@ -0,0 +1,13 @@
+import json
+import gzip
+import re
+
+with gzip.open('jawiki-country.json.gz', 'r') as f:
+    for line in f:
+        data = json.loads(line)
+        if data['title'] == 'イギリス':
+            text = data['text']
+            break
+
+pattern = r'\[\[ファイル:(.*?\..*?)\|'
+print('\n'.join(re.findall(pattern,text)))
