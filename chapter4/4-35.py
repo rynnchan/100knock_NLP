2d72f17fe9ef3d7add29105b891fef57a0c46a9d
rynnchan <ogw334@gmail.com>
2020-07-10 18:43:11 +0900

    chapter4-35

---
+++ chapter4/4-35.py
@@ -0,0 +1,32 @@
+import collections
+
+with open('neko.txt.mecab') as f:
+    s = f.read().splitlines()
+    token_list = []
+    token = []
+    for morpheme in s:
+        if morpheme == 'EOS':
+            if token != []:
+                token_list.append(token)
+                token = []
+            continue
+        elif morpheme == '':
+            continue
+        morpheme_sp = morpheme.split(',')
+        sur_spe = morpheme_sp[0].split('\t')
+        if sur_spe[0] == '':
+            continue
+        token.append(
+            dict(
+                surface = sur_spe[0],
+                base = morpheme_sp[6],
+                pos = sur_spe[1],
+                pos1 = morpheme_sp[1]
+            )
+        )
+words = []
+for l in token_list:
+    for morpheme in l:
+        words.append(morpheme['surface'])
+c = collections.Counter(words)
+print(c.most_common())
