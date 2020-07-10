3a06f0872e4c7ad657863737e7a7d98ec413cfbf
rynnchan <ogw334@gmail.com>
2020-07-10 18:43:17 +0900

    chapter4-36

---
+++ chapter4/4-36.py
@@ -0,0 +1,36 @@
+import collections
+import matplotlib.pyplot as plt
+
+with open('neko.txt.mecab') as f:
+    s = f.read().splitlines()
+    token_list = []
+    token = []
+    for morpheme in s:
+        if morpheme == 'EOS':
+            if token != []:
+                token_list.append(token)
+                token = []
+            continue
+        elif morpheme == '':
+            continue
+        morpheme_sp = morpheme.split(',')
+        sur_spe = morpheme_sp[0].split('\t')
+        if sur_spe[0] == '':
+            continue
+        token.append(
+            dict(
+                surface = sur_spe[0],
+                base = morpheme_sp[6],
+                pos = sur_spe[1],
+                pos1 = morpheme_sp[1]
+            )
+        )
+words = []
+for l in token_list:
+    for morpheme in l:
+        words.append(morpheme['surface'])
+c = collections.Counter(words)
+keys, counts = zip(*c.most_common()[:10])
+plt.rcParams["font.family"] = "IPAexGothic"
+plt.bar(keys, counts,width=0.9)
+plt.show()
